ce112148d199c797855a1523c02948dd9063a386
Anna-Biryukova <139253733+Anna-Biryukova@users.noreply.github.com>
2023-11-02 17:15:06 +0300

    auth & reg release 1.0

--- client/src/services/apiService.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const apiService = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL as string,
-});
-
-
-export default apiService
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const session = require("express-session");
 const FileStore = require("session-file-store")(session);
 const userRouter = require('./routes/userRouter');
+const jwt = require('jsonwebtoken');
 
 
 const PORT = process.env.PORT || 3000;
@@ -15,19 +16,45 @@ app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
-app.use(
-  session({
-    name: "sid",
-    secret: process.env.SESSION_SECRET ?? "test",
-    resave: true,
-    store: new FileStore(),
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 12,
-      httpOnly: true,
-    },
-  })
-);
+// app.use(
+//   session({
+//     name: "sid",
+//     secret: process.env.SESSION_SECRET ?? "test",
+//     resave: true,
+//     store: new FileStore(),
+//     saveUninitialized: false,
+//     cookie: {
+//       maxAge: 1000 * 60 * 60 * 12,
+//       httpOnly: true,
+//     },
+//   })
+// );
+
+function generateToken(data) {
+  return jwt.sign(data, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+}
+
+app.use(express.json());
+
+// const authenticateJWT = (req, res, next) => {
+//   const authHeader = req.headers.authorization;
+
+//   if (authHeader) {
+//       const token = authHeader.split(' ')[1];
+
+//       jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+//           if (err) {
+//               return res.sendStatus(403);
+//           }
+
+//           req.user = user;
+//           next();
+//       });
+//   } else {
+//       res.sendStatus(401);
+//   }
+// };
+
 
 app.use('/api/user', userRouter);
 
