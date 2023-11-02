671a13f0b41898853df2ec692e26b66e4ccc174c
Artur Agafontsev <dingodevelop@gmail.com>
2023-11-02 17:35:28 +0300

    all-done-map-footer-page-reception

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import ReceptionPointsPage from './components/Pages/ReceptionPointsPage';
+import MainPage from './components/Pages/MainPage';
+import Layout from './components/Layout';
+
+export default function App(): JSX.Element {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/reception" element={<ReceptionPointsPage />} />
+      </Route>
+    </Routes>
+  );
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
-import store from './features/redux/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <ChakraProvider>
     <BrowserRouter>
-      <Provider store={store}>
         <App />
-      </Provider>
     </BrowserRouter>
-  </ChakraProvider>,
 );
