49817a71c272f12aabde8d9f508b8082d7843220
Anna-Biryukova <139253733+Anna-Biryukova@users.noreply.github.com>
2023-11-08 20:30:24 +0300

    Main Page with ideas

--- client/src/features/redux/thunkActions/ideaThunkAction.ts
+++ client/src/features/redux/thunkActions/ideaThunkAction.ts
@@ -1,23 +1,18 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { deleteIdeas, getIdeas, submitIdeas, updateIdeas } from '../../../services/ideaService'
-import type { CardIdeaFormType, CardIdeaType } from '../../../types/ideaTypes'
+import {  getIdeas } from '../../../services/ideaService'
+import type { CardIdeaType } from '../../../types/ideaTypes'
 
 export const getIdeaThunk = createAsyncThunk<CardIdeaType[]>(
   'idea/getIdeaThunk',
   async () => getIdeas(),
 );
 
-export const addIdeaThunk = createAsyncThunk<CardIdeaType, CardIdeaFormType>(
-  'idea/addIdeaThunk',
-  async (formData) => submitIdeas(formData),
-);
+// export const addIdeaThunk = createAsyncThunk<CardIdeaType, CardIdeaFormType>(
+//   'idea/addIdeaThunk',
+//   async (formData) => submitIdeas(formData),
+// );
 
-export const updateIdeaThunk = createAsyncThunk<
-CardIdeaType,
-  { id: CardIdeaType['id']; formData: CardIdeaFormType }
->('idea/updateIdeaThunk', async ({ id, formData }) => updateIdeas(id, formData));
-
-export const deleteIdeaThunk = createAsyncThunk<
-CardIdeaType['id'],
-  { id: CardIdeaType['id'] }
->('idea/deleteIdeaThunk', async ({ id }) => deleteIdeas(id));
+// export const deleteIdeaThunk = createAsyncThunk<
+// CardIdeaType['id'],
+//   { id: CardIdeaType['id'] }
+// >('idea/deleteIdeaThunk', async ({ id }) => deleteIdeas(id));
